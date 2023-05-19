bc0ed755d2483028085c44e869ffc34bdac9e7d7
HRISHANK SANJAY SHIRODKAR <hrishanksanjayshirodkar@HRISHANKs-MacBook-Air.local>
2023-05-19 11:48:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect } from 'react';
+import React from 'react';
+import MovieCard from "./MovieCard";
+import SearchIcon from "./search.svg";
 
-function App() {
+// const API_URL = "https://www.ombdapi.com?apikey=ea75e6ff"
+const API_URL = 'http://localhost:3000/http://www.omdbapi.com/?i=tt3896198&apikey=ea75e6ff';
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = response.json();
+    console.log(data)
+    data.then((res) => {
+        setMovies(res.Search);
+    });
+}
+
+useEffect(() => {
+    searchMovies('spiderman');
+}, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for movies"
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
